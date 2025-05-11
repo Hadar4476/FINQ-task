2a811ee2c4a3f003b228e284b64d1b300bbd19de
Hadar4476 <hadar4476@gmail.com>
2025-05-11 13:27:52 +0300

    initialized database and added CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,23 @@
 import express from "express";
-
 import bodyParser from "body-parser";
+import cors from "cors";
+
+import config from "./config";
+
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+mongoose
+  .connect(config.MONGO_URI)
+  .then(async () => {
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
