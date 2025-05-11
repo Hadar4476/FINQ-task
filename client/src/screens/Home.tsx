5c5e42d9b0a92d0e9dca6ed0d572a4c969ce6db1
Hadar4476 <hadar4476@gmail.com>
2025-05-11 16:23:03 +0300

    finished home screen

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="d-flex justify-content-center align-items-center min-vh-100">
+      <div className="d-flex gap-3">
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate("/randomUsers")}
+        >
+          Fetch
+        </button>
+        <button
+          className="btn btn-success"
+          onClick={() => navigate("/history")}
+        >
+          History
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
